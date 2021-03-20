72d27531f051aa66f7e2739667a93e6d52a19c23
hritikchokker <hchokker7@gmail.com>
2021-03-21 00:04:07 +0530

    app spec version test update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { TestBed } from '@angular/core/testing';
+import { VERSION } from '@angular/core';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -19,17 +20,15 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'test-guide'`, () => {
+  it(`should have app version to be ${VERSION.full}`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('test-guide');
+    expect(app.version).toEqual(VERSION);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-guide app is running!');
-  });
+  // it(`should have as title 'test-guide'`, () => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.componentInstance;
+  //   expect(app.title).toEqual('test-guide');
+  // });
 });
